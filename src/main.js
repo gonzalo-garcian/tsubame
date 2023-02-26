59de577c5ca330c8d5cdb66b540dbc8d8e0662c9
garcian-gonzalo <44837677+gonzalo-garcian@users.noreply.github.com>
2023-02-26 13:34:44 +0100

    Initial commit

--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@ import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
+import VueKonva from 'vue-konva';
 
 import "./assets/main.css";
 
 const app = createApp(App);
 
 app.use(createPinia());
+app.use(VueKonva);
 app.use(router);
 
 app.mount("#app");
